3dc0a3438c3383c1cf00df2feedf86f808055cd7
khingxc <khingkhwanc@gmail.com>
2022-06-04 00:06:07 +0700

    fix: using ImageMagick

--- script/make_thumbnail.sh
+++ script/make_thumbnail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-palette="/tmp/palette.png"
-filters="scale=320:-1:flags=lanczos"
-
-ffmpeg -v warning -i "$1" -vf "$filters,palettegen" -y $palette
-ffmpeg -v warning -i "$1" -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -t 00:00:10 -y $2
+mkdir frames                                                                # creating a dir named frames
+output_name="${2%.*}"                                                       # getting output name
+ffmpeg -i $1 -t 00:00:10 -r 30/1 frames/frame%03d.jpg                       # extracting 10s video into frames stored in the brandnew dir named frames
+convert -resize 720x540 -delay 0 -loop 0 frames/*.jpg $output_name.gif      # making a gif
+rm -r frames                                                                # removing everything in frames dir and the dir itself
